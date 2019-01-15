30ffc3aa769616fd4f9bbe2daeeba1e6be8adc85
Samak Twins <samaktwins@gmail.com>
2019-01-15 14:21:16 +0530

    Add files via upload

---
+++ Master_LED.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(2,3); // RX,TX
+
+void setup() {
+pinMode(13, OUTPUT);
+Serial.begin(9600);
+BTSerial.begin(38400);
+}
+
+void loop() {
+  
+  digitalWrite(13,HIGH);
+  BTSerial.println("1");
+  delay(5000);
+
+  digitalWrite(13,LOW);
+  BTSerial.println("0");
+  delay(5000);
+}
---
+++ Slave_LED.ino
@@ -0,0 +1,20 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  Serial.begin(38400);
+  pinMode(9,OUTPUT);      // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode
+  digitalWrite(9,HIGH);
+  }
+
+void loop() {
+  if(Serial.available()>0);{
+  
+    char x=Serial.read();
+
+    if(x=='1'){
+       digitalWrite(13,HIGH);
+    }
+    if(x=='0'){
+      digitalWrite(13,LOW);
+    }
+  }
+}
